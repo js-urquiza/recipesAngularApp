57a4f415fd0bd921e65801e4aa597dd8161e7604
juansantiago <js.urquiza@gmail.com>
2023-01-17 18:41:01 -0300

    Add structure of recipe detail

--- recipesApp/src/app/recipes/recipes-list/recipes-list.component.ts
+++ recipesApp/src/app/recipes/recipes-list/recipes-list.component.ts
@@ -9,7 +9,6 @@ import { Recipe } from '../recipe.model';
 export class RecipesListComponent {
   recipes: Recipe[] = [
     new Recipe('Una receta', 'Esto es un simple test', 'https://images.unsplash.com/photo-1466637574441-749b8f19452f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80'),
-    
   ];
 
   constructor() {
